5627fe80b9213dd2cd0103a1c79b7bfdb986a870
Kirill GPRB <41549137+undnull@users.noreply.github.com>
2022-12-01 03:06:17 +0500

    OH MY GOD IT IS 1.7 GiB

---
+++ nether_roof/custom.markers.js
@@ -0,0 +1,1 @@
+UnminedCustomMarkers={isEnabled:true,markers:[{x:0.5,z:9,image:"custom.worldspawn.png",imageAnchor:[0.5,1.0],imageScale:0.25}]}
---
+++ the_end/custom.markers.js
@@ -0,0 +1,1 @@
+UnminedCustomMarkers={isEnabled:true,markers:[{x:0.5,z:1,image:"custom.worldspawn.png",imageAnchor:[0.5,1.0],imageScale:0.25}]}
